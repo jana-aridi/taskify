fdd3d903a2225846d41cd72695c967efb00e65f2
janaaridi <j.aridi@aems.ai>
2023-11-21 14:56:31 +0200

    protected the routes  + moved imgs & loc stor file

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,29 @@ import {Route, Routes, Navigate} from 'react-router-dom';
 import Signup from './Components/Signup';
 import Login from './Components/Login';
 import Home from './Pages/UserHome/Home';
+import AdminPanel from './Pages/AdminPanel/AdminPanel';
 
 function App() {
-  const user = localStorage.getItem('token');
-  console.log(user)
+
+  const user = localStorage.getItem('user'); 
+
   return (
-    <Routes>
-      
-      {user && !user.isAdmin && <Route path="/" exact element={<Home/>} />}
-      {/* {user && user.isAdmin && <Route path="/" exact element={<Home/>} />} */}
-      <Route path="/" exact element={<Navigate replace to='/login'/>}/> 
+    <Routes> 
+
+            
       <Route path="/login" exact element={<Login/>}/>
-      <Route path="/signup" exact element={<Signup/>}/>
-      <Route path="/home" exact element={<Home/>}/>
+      <Route path="/signup" exact element={<Signup/>}/> 
+      
+      {user && <Route path="/home" exact element={<Home/>} />}
+      {user && <Route path="/admin-panel" exact element={<AdminPanel/>} />}
+
+      {!user && <Route path="/" exact element={<Login/>}/>}
+
+      {user && user.isAdmin && <Route path="/" exact element={<AdminPanel/>}/>}
+      {user && !user.isAdmin && <Route path="/" exact element={<Home/>}/>}
+
+      {user && user.isAdmin && <Route path="/home" exact element={<AdminPanel/>}/>}
+      {user && !user.isAdmin && <Route path="/admin-panel" exact element={<Home/>}/>}
     </Routes>
   );
 }
